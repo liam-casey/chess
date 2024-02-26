13f2f30dff7a85f1b58d396922b1bf74151b717a
liam-casey <123138023+liam-casey@users.noreply.github.com>
2024-02-26 16:18:58 -0500

    updated square class

---
+++ square.py
@@ -0,0 +1,19 @@
+#This is a square on a chess board, a square has a boolean white
+#a boolean Occupied, and a piece that specifies which piece is on it
+#a square also has a string location that defines its position on the board. 
+
+class Square:
+    def __init__(self, location, isWhite):
+        self.location = location
+        self.isWhite = isWhite  # Assign the passed value to the isWhite attribute
+        self.occupied = False
+        self.piece = None
+        
+    def occupy(self, piece):
+        self.occupied = True
+        self.piece = piece
+
+    def vacate(self):
+        self.occupied = False
+        self.piece = None
+
