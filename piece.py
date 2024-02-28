c8b2957d4b5a79877fcf74d6f76a6e106b54c4ce
Ari Menezes <ihorus11@hotmail.com>
2024-02-28 15:55:48 -0500

    example of king instance

--- piece.py
+++ piece.py
@@ -1,7 +1,7 @@
 
-
+# Location is a 2 length tuple in format (x, y)
 class Piece():
-    def __init__(self,location, moves, image):
+    def __init__(self, location, image, color):
+        self.color = color
         self.location = location
-        self.moves = moves
         self.image = image
--- pieces/king.py
+++ pieces/king.py
@@ -2,4 +2,10 @@ from piece import Piece
 class King(Piece):
     def __init__(self, location, moves, image):
         super().__init__(location, moves, image)
+        self.has_moved = False
 
+    def trigger_move(self, new_location):
+        d_x = abs(new_location[0] - self.location[0])
+        d_y = abs(new_location[1] - self.location[1])
+        if (2 > d_x >= 0) and (2 > d_y >=0):
+            self.has_moved = True
