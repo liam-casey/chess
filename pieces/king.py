7cf74d03ad1b582d1dcbf06acb19d8b102233d13
Ari Menezes <ihorus11@hotmail.com>
2024-02-28 16:00:22 -0500

    castling exception

--- pieces/king.py
+++ pieces/king.py
@@ -4,8 +4,20 @@ class King(Piece):
         super().__init__(location, moves, image)
         self.has_moved = False
 
-    def trigger_move(self, new_location):
+    def trigger_move(self, new_location, is_castle=False):
         d_x = abs(new_location[0] - self.location[0])
         d_y = abs(new_location[1] - self.location[1])
+        if is_castle:
+            if self.has_moved:
+                return False
+            else:
+                # assume the board is valid for a castle
+                self.location = new_location
+                return True
         if (2 > d_x >= 0) and (2 > d_y >=0):
             self.has_moved = True
+            self.location = new_location
+            return True
+
+
+
