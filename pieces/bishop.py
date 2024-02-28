46ebcc7561b2e31eba36dcfae3d8b455419747fb
Sebas-Thomas <sothomas@uvm.edu>
2024-02-28 16:01:34 -0500

    Started bishop class

--- pieces/bishop.py
+++ pieces/bishop.py
@@ -1,5 +1,10 @@
-class Bishop:
-    def __init__(self,location, moves, image):
-        self.location = location
-        self.moves = moves
-        self.image = image
+from piece import Piece
+class Bishop(Piece):
+    def __init__(self,location, image):
+        super().__init(location, image)
+        self.has_moved = False
+    
+    def check_move(self, new_location):
+        # equation checks piece movements
+        d_x = abs(new_location[]) - 
+        d_y = abs(new_location[] - self.location[1])
