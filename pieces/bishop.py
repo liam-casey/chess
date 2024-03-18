a5271730374bb225df54afdc14a3dc6a052421f2
liam <lcasey1@uvm.edu>
2024-03-18 16:11:15 -0400

    fixed bishop class

--- pieces/bishop.py
+++ pieces/bishop.py
@@ -1,22 +1,25 @@
 from piece import Piece
 class Bishop(Piece):
-    def __init__(self,location, image, color):
-        # TODO get rid of color?
-        super().__init__(location, image, color)
-        self.has_moved = False
+   def __init__(self,location, image, color):
+       # TODO get rid of color?
+       super().__init__(location, image, color)
+       self.has_moved = False
 
-    def move(self, new_location):
-        # equation checks piece movements
-        d_x = abs(new_location[0]) - self.location[0] 
-        d_y = abs(new_location[1] - self.location[1])
-        # checks to see if move is diagonal or no
-        if d_x == d_y:
+
+   def move(self, new_location):
+       # equation checks piece movements
+       d_x = abs(new_location[0]) - self.location[0]
+       d_y = abs(new_location[1] - self.location[1])
+       # checks to see if move is diagonal or no
+       if abs(d_x) == abs(d_y):
             self.location = new_location
+            self.has_moved = True
             return True
-        else:
-            return False
+       return False;    
+      
 
-    # function takes in position after a piece has been moved and reassigns the ending location
-    # as the current location
-    def update_location(self, endPos):
-        self.location = endPos
+
+   # function takes in position after a piece has been moved and reassigns the ending location
+   # as the current location
+   def update_location(self, endPos):
+       self.location = endPos
