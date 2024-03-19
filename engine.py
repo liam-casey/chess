e8168a9da3b3ea5cba3e1f7ef6f7fb926e07d830
Brandon Wolf <brandon.wolf@uvm.edu>
2024-03-18 20:14:14 -0400

    add rook collision property, can't move past other pieces

--- engine.py
+++ engine.py
@@ -26,6 +26,8 @@ class GameState:
             return
         if endPos[0] > 7 or endPos[0] > 7:
             return
+        if startPos == endPos:
+            return
         # check to see if the piece is the right color
         piece = self.board.getPiece(startPos)
         # get the piece at the end position
@@ -41,10 +43,77 @@ class GameState:
         # if piece is the right color, do the move and update the board, otherwise print something out
         if self.whiteToMove and piece.get_color() == "white":
             if piece.move(endPos):
-                # Then do this stuff
+                # This code makes sure that the rook can't move through pieces
                 if isinstance(piece, Rook):
-                    dx = endPos[0] - startPos[0]
-                    dy = endPos[1] - startPos[1]
+                    dy = abs(endPos[0] - startPos[0])
+                    dx = abs(endPos[1] - startPos[1])
+                    if dy == 0:
+                        count = 1
+                        while count < dx:
+                            tup = (endPos[0], max(startPos[1], endPos[1]) - count)
+                            if self.board.getPiece(tup) != "":
+                                print(tup)
+                                return
+                            count = count + 1
+                    else:
+                        count = 1
+                        while count < dy:
+                            tup = (max(startPos[1], endPos[1]) - count, endPos[1])
+                            if self.board.getPiece(tup) != "":
+                                print(tup)
+                                return
+                            count = count + 1
+                # if isinstance(piece, Bishop):
+                #     x_cord = endPos[0]
+                #     y_cord = endPos[1]
+                #     while x_cord > startPos[0] - 1:
+                #         if self.board.getPiece((x_cord, y_cord)) != "":
+                #             return
+                #         x_cord = x_cord - 1
+                #         y_cord = y_cord - 1
+                # if isinstance(piece, Queen):
+                #     dx = abs(endPos[0] - startPos[0])
+                #     dy = abs(endPos[1] - startPos[1])
+                #     if dx != 0 and dy != 0:
+                #         x_cord = endPos[0]
+                #         y_cord = endPos[1]
+                #         while x_cord > startPos[0] - 1:
+                #             if self.board.getPiece((x_cord, y_cord)) != "":
+                #                 return
+                #             x_cord = x_cord - 1
+                #             y_cord = y_cord - 1
+                #     elif dx == 0:
+                #         i = endPos[0]
+                #         while i > startPos[0] - 1:
+                #             if self.board.getPiece((i, startPos[1])) != "":
+                #                 return
+                #             i = i - 1
+                #     else:
+                #         i = endPos[1]
+                #         while i > startPos[1] - 1:
+                #             if self.board.getPiece((startPos[0], i)) != "":
+                #                 return
+                #             i = i - 1
+                # # king is only allowed to move one space, so if it trys to take an ally piece, it will catch up above
+                # # if isinstance(piece, King):
+                # #     pass
+                # if isinstance(piece, Pawn):
+                #     pass
+                self.board.updateBoard(startPos, endPos)
+                self.whiteToMove = False
+                self.blackTaken.append(takenPiece)
+                return
+            # else print bad move and return
+            else:
+                # then print something out that says its not a good move
+                print ("This move doesn't work")
+                return
+        elif self.whiteToMove == False and piece.get_color() == "black":
+            # also update this code to update the pieces position, could do this in board as well.
+            if piece.move(endPos):
+                if isinstance(piece, Rook):
+                    dx = startPos[0] - endPos[0]
+                    dy = startPos[1] - endPos[1]
                     if dx == 0:
                         i = endPos[0]
                         while i > startPos[0] - 1:
@@ -58,34 +127,40 @@ class GameState:
                                 return
                             i = i - 1
                 if isinstance(piece, Bishop):
-                    pass
+                    x_cord = endPos[0]
+                    y_cord = endPos[1]
+                    while x_cord > startPos[0] - 1:
+                        if self.board.getPiece((x_cord, y_cord)) != "":
+                            return
+                        x_cord = x_cord - 1
+                        y_cord = y_cord - 1
                 if isinstance(piece, Queen):
-                    pass
-                if isinstance(piece, King):
-                    pass
+                    dx = endPos[0] - startPos[0]
+                    dy = endPos[1] - startPos[1]
+                    if dx != 0 and dy != 0:
+                        x_cord = endPos[0]
+                        y_cord = endPos[1]
+                        while x_cord > startPos[0] - 1:
+                            if self.board.getPiece((x_cord, y_cord)) != "":
+                                return
+                            x_cord = x_cord - 1
+                            y_cord = y_cord - 1
+                    elif dx == 0:
+                        i = endPos[0]
+                        while i > startPos[0] - 1:
+                            if self.board.getPiece((i, startPos[1])) != "":
+                                return
+                            i = i - 1
+                    else:
+                        i = endPos[1]
+                        while i > startPos[1] - 1:
+                            if self.board.getPiece((startPos[0], i)) != "":
+                                return
+                            i = i - 1
+                # king is only allowed to move one space, so if it trys to take an ally piece, it will catch up above
+                # if isinstance(piece, King):
+                #     pass
                 if isinstance(piece, Pawn):
-                    pass
-                self.board.updateBoard(startPos, endPos)
-                self.whiteToMove = False
-                self.blackTaken.append(takenPiece)
-                return
-            # else print bad move and return
-            else:
-                # then print something out that says its not a good move
-                print ("BAD MOVE")
-                return
-        elif self.whiteToMove == False and piece.get_color() == "black":
-            # also update this code to update the pieces position, could do this in board as well.
-            if piece.move(endPos):
-                if piece.isinstance(Rook):
-                    pass
-                if piece.isinstance(Bishop):
-                    pass
-                if piece.isinstance(Queen):
-                    pass
-                if piece.isinstance(King):
-                    pass
-                if piece.isinstance(Pawn):
                     pass
                 self.board.updateBoard(startPos, endPos)
                 self.whiteToMove = True
